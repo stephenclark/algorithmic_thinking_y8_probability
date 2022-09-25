8d6bacc3656784e7d8a11701b3717dce17bf2832
Stephen Kennedy-Clark <stephenclark@users.noreply.github.com>
2022-09-25 20:27:47 +1000

    Create two_heads.py

---
+++ two_heads.py
@@ -0,0 +1,19 @@
+# models flipping two coins X number of times and counts the number
+# of double heads
+import random
+
+number_of_flips = 150
+number_of_two_heads = 0
+
+def flip_two_coins():
+    return (random.choice(["Heads","Tails"]), random.choice(["Heads","Tails"]))
+
+for i in range(0,number_of_flips):
+    flip = flip_two_coins()
+    if flip == ("Heads", "Heads"):
+        number_of_two_heads += 1
+   
+   
+    
+print(".....")
+print("{} Double 'Heads' in  {} flips: {}".format(number_of_two_heads,number_of_flips, number_of_two_heads/number_of_flips))
