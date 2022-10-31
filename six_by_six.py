a709c7873bdd9d2729a15ed2b70ea61403ec49ec
Stephen Kennedy-Clark <stephenclark@users.noreply.github.com>
2022-10-31 12:52:02 +1000

    Add files via upload
    Simulates all the possible addition of two six sided dice rolls

---
+++ six_by_six.py
@@ -0,0 +1,18 @@
+# set up the confusion matrix for  6x6 dice
+# calculate what the numbers add to
+R = C = 6
+# Initialize matrix 
+matrix = []
+results = [] 
+for i in range(R):          # A for loop for row entries 
+    a =[] 
+    for j in range(C):      # A for loop for column entries 
+         a.append("({}, {} = {})".format(i+1,j+1, i+j+2))
+         results.append(i+j+2)
+    matrix.append(a) 
+
+for line in matrix:
+    print ('  '.join(map(str, line)))
+
+for i in range(2,max(results)+1):
+    print("number of {}'s = {}".format(i, results.count(i)))
